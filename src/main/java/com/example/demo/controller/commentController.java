f3148726782a3cb404db0a3229c4b87f9429f7ca
hthcccc <65483560+hthcccc@users.noreply.github.com>
2022-01-03 17:51:49 +0800

    评论优化

--- src/main/java/com/example/demo/controller/commentController.java
+++ src/main/java/com/example/demo/controller/commentController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.config.ApiGroup;
 import com.example.demo.model.Comment;
+import com.example.demo.result.Result;
 import com.example.demo.service.CommentService;
 import com.example.demo.controller.resultBody.*;
 import io.swagger.annotations.ApiOperation;
@@ -20,57 +21,57 @@ public class commentController {
     @Autowired
     CommentService tmp;
 
-    @GetMapping("/getText")
-    @ApiGroup(group = {"comment"})
-    @ApiOperation(value = "获取评论文字信息",notes = "评论id，层数")
-    public String getText(String comment_id,Integer level){
-        return tmp.getText(comment_id,level);
-    }
+//    @GetMapping("/getText")
+//    @ApiGroup(group = {"comment"})
+//    @ApiOperation(value = "获取评论文字信息",notes = "评论id，层数")
+//    public String getText(String comment_id,Integer level){
+//        return tmp.getText(comment_id,level);
+//    }
 
     @GetMapping("/getComment")
     @ApiGroup(group = {"comment"})
     @ApiOperation(value = "获取评论信息",notes = "评论id，层数")
-    public Comment getComment(String comment_id, Integer level){
+    public Result getComment(String comment_id, Integer level){
         return tmp.getComment(comment_id,level);
     }
 
     @PostMapping("/newComment")
     @ApiGroup(group = {"comment"})
     @ApiOperation(value = "新建评论",notes = "用户id，商品id，文字信息")
-    public void newComment(@RequestParam("user_id") String user_id,
+    public Result newComment(@RequestParam("user_id") String user_id,
                            @RequestParam("good_id") String good_id,
                            @RequestParam("text") String text){
-        tmp.newComment(user_id,good_id,text);
+        return tmp.newComment(user_id,good_id,text);
     }
 
     @PostMapping("/replyComment")
     @ApiGroup(group = {"comment"})
     @ApiOperation(value = "回复评论",notes = "评论id，用户id，文字信息")
-    public void replyComment(@RequestParam("comment_id") String comment_id,
+    public Result replyComment(@RequestParam("comment_id") String comment_id,
                            @RequestParam("user_id") String user_id,
                            @RequestParam("text") String text){
-        tmp.addComment(comment_id,user_id,text);
+        return tmp.addComment(comment_id,user_id,text);
     }
 
     @PostMapping("/deleteComment")
     @ApiGroup(group = {"comment"})
     @ApiOperation(value = "删除评论",notes = "评论id，层数")
-    public void deleteComment(@RequestParam("comment_id") String comment_id,
+    public Result deleteComment(@RequestParam("comment_id") String comment_id,
                              @RequestParam("level") Integer level){
-        tmp.deleteComment(comment_id,level);
+        return tmp.deleteComment(comment_id,level);
     }
 
     @GetMapping("/getAllByGood/{good_id}")
     @ApiGroup(group = {"good","comment"})
     @ApiOperation(value = "获取商品的所有评论（第一层）",notes = "商品id")
-    public List<commentListBody> getAllByGood(@PathVariable String good_id){
+    public Result getAllByGood(@PathVariable String good_id){
         return tmp.getAllByGood(good_id);
     }
 
     @GetMapping("/listAllBelow/{comment_id}")
     @ApiGroup(group = {"comment"})
     @ApiOperation(value = "查看某条评论的所有回复",notes = "评论id")
-    public List<commentListBody> listAllBelow(@PathVariable String comment_id){
+    public Result listAllBelow(@PathVariable String comment_id){
         return tmp.listAllBelow(comment_id);
     }
 }
--- src/main/java/com/example/demo/service/CommentService.java
+++ src/main/java/com/example/demo/service/CommentService.java
@@ -6,15 +6,15 @@ import com.example.demo.model.CommentId;
 import com.example.demo.repository.commentRepository;
 import com.example.demo.repository.goodRepository;
 import com.example.demo.repository.userRepository;
+import com.example.demo.result.Result;
+import com.example.demo.result.ResultFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
 import java.time.Instant;
 import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @Service
 public class CommentService implements IDGenenrator{
@@ -25,16 +25,29 @@ public class CommentService implements IDGenenrator{
     @Autowired
     goodRepository goodRepo;
 
-    public String getText(String comment_id,Integer level){
-        return commentRepo.getText(comment_id,level);
+//    public String getText(String comment_id,Integer level){
+//        return commentRepo.getText(comment_id,level);
+//    }
+
+    public Result getComment(String comment_id,Integer level) {
+        if(!commentRepo.existsById(new CommentId(comment_id,level))){
+            return ResultFactory.buildFailResult("不存在该留言");
+        }
+        Comment comment = commentRepo.getComment(comment_id,level);
+        Map<String,Object> result=new HashMap<>();
+        result.put("comment_id",comment.getId().getCommentId());
+        result.put("comment_level",comment.getId().getLevel());
+        result.put("date",comment.getDate());
+        result.put("text",comment.getText());
+        result.put("user_id",comment.getUserId());
+        result.put("user_name",userRepo.findById(comment.getUserId()).get().getName());
+        return ResultFactory.buildSuccessResult(result);
     }
 
-    public Comment getComment(String comment_id,Integer level){
-        return commentRepo.getComment(comment_id,level);
-    }
-
-    public void newComment(String u_id,String g_id,String text){
-        if(!userRepo.existsById(u_id)||!goodRepo.existsById(g_id)||(text==null)||text.isEmpty()){return;}
+    public Result newComment(String u_id,String g_id,String text){
+        if(!userRepo.existsById(u_id)||!goodRepo.existsById(g_id)||(text==null)||text.isEmpty()){
+            return ResultFactory.buildFailResult("传入参数不存在");
+        }
         Comment comment=new Comment();
         comment.setId(generateID(10),0);
         //CommentId id=new CommentId();
@@ -46,12 +59,13 @@ public class CommentService implements IDGenenrator{
         comment.setGoodId(g_id);
         comment.setUserId(u_id);
         commentRepo.save(comment);
+        return ResultFactory.buildResult(200,"新建评论成功",null);
         //commentRepo.newComment(generateID(10),u_id,g_id,
         //        text,Instant.now(),0);
     }
 
-    public void addComment(String c_id,String u_id,String text){
-        if(commentRepo.ifExistsByCommentID(c_id)==0){return;}
+    public Result addComment(String c_id,String u_id,String text){
+        if(commentRepo.ifExistsByCommentID(c_id)==0){return ResultFactory.buildFailResult("不存在该留言");}
         Comment comment=new Comment();
         comment.setId(c_id,commentRepo.getCurrentLevel(c_id)+1);
         comment.setUserId(u_id);
@@ -61,37 +75,47 @@ public class CommentService implements IDGenenrator{
 //        commentRepo.newComment(c_id,u_id,commentRepo.getGoodIdByCommentId(c_id),
 //                text,Instant.now(),commentRepo.getCurrentLevel(c_id)+1);
         commentRepo.save(comment);
+        return ResultFactory.buildResult(200,"回复成功",null);
     }
 
-    public List<commentListBody> getAllByGood(String good_id){
+    public Result getAllByGood(String good_id){
         if(goodRepo.existsById(good_id)) {
             List<Comment> commentList = commentRepo.getAllbyGood(good_id);
-            List<commentListBody> result = new ArrayList<commentListBody>();
-            for (Comment c : commentList) {
-                commentListBody element = new commentListBody(c.getId().getCommentId(),
-                        userRepo.getName(c.getUserId()), c.getText(), c.getDate());
-                result.add(element);
+            List<Map<String,Object>> result = new ArrayList<>();
+            for (Comment comment : commentList) {
+                Map<String,Object> map=new HashMap<>();
+                map.put("comment_id",comment.getId().getCommentId());
+                map.put("time",comment.getDate());
+                map.put("text",comment.getText());
+                map.put("user_id",comment.getUserId());
+                map.put("user_name",userRepo.findById(comment.getUserId()).get().getName());
+                result.add(map);
             }
-            return result;
+            return ResultFactory.buildSuccessResult(result);
         }
-        return null;
+        return ResultFactory.buildFailResult("商品不存在");
     }
 
-    public List<commentListBody> listAllBelow(String comment_id){
+    public Result listAllBelow(String comment_id){
         if(commentRepo.ifExistsByCommentID(comment_id)==1){
-            List<commentListBody> result=new ArrayList<commentListBody>();
+            List<Map<String,Object>> result=new ArrayList<>();
             List<Comment> commentList = commentRepo.getAllBehind(comment_id);
-            for(Comment c:commentList){
-                result.add(new commentListBody(comment_id,
-                        userRepo.getName(c.getUserId()),
-                        c.getText(),c.getDate()));
+            for(Comment comment:commentList){
+                Map<String,Object> map=new HashMap<>();
+                map.put("text",comment.getText());
+                map.put("date",comment.getDate());
+                map.put("user_id",comment.getUserId());
+                map.put("user_name",userRepo.findById(comment.getUserId()).get().getName());
+                map.put("comment_id",comment.getId().getCommentId());
+                map.put("comment_level",comment.getId().getLevel());
+                result.add(map);
             }
-            return result;
+            return ResultFactory.buildSuccessResult(result);
         }
-        return null;
+        return ResultFactory.buildFailResult("评论不存在");
     }
 
-    public void deleteComment(String comment_id,Integer level){
+    public Result deleteComment(String comment_id,Integer level){
         if(commentRepo.existsById(new CommentId(comment_id,level))){
             if(level>0) {
                 commentRepo.delete(commentRepo.findById(new CommentId(comment_id, level)).get());
@@ -99,7 +123,9 @@ public class CommentService implements IDGenenrator{
             }else{
                 commentRepo.removeComment(comment_id);
             }
+            return ResultFactory.buildResult(200,"删除成功",null);
         }
+        return ResultFactory.buildFailResult("不存在该留言");
     }
 
     @Override
