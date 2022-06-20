b2bf3ff1a2d268ed115f0b48cc354adbcdad9d35
hthcccc <65483560+hthcccc@users.noreply.github.com>
2022-06-20 12:13:12 +0800

    测试通过用户名返回用户列表通过

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.Test.UserTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -13,6 +13,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -43,6 +45,27 @@ class UserServiceTest {
 
     @Test
     void getUsersByName() {
+        List<User> users = (List<User>) userService.getUsersByName("黄天浩").getObject();
+        if(users.size()>0) {
+            for(User user:users) {
+                Assert.assertEquals("黄天浩", user.getName());
+            }
+        }else{
+            Assert.assertTrue(users.size()==0);
+        }
+
+        users = (List<User>) userService.getUsersByName("胡劲松").getObject();
+        if(users.size()>0) {
+            for(User user:users) {
+                Assert.assertEquals("胡劲松", user.getName());
+                Assert.assertFalse("黄天浩".equals(user.getName()));
+            }
+        }else{
+            Assert.assertTrue(users.size()==0);
+        }
+
+        users=(List<User>)userService.getUsersByName("越前龙马").getObject();
+        Assert.assertTrue(users.size()==0);
     }
 
     @Test
