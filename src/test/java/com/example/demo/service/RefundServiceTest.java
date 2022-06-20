7b256f300d0dbb640300b5169425f8f742652573
hthcccc <65483560+hthcccc@users.noreply.github.com>
2022-06-21 02:11:21 +0800

    退款单元测试 完成

--- src/test/java/com/example/demo/service/RefundServiceTest.java
+++ src/test/java/com/example/demo/service/RefundServiceTest.java
@@ -76,37 +76,63 @@ class RefundServiceTest {
         //首先造一条退款申请
         String order_id = "6631065311033663";
         refundService.submitRefund(order_id,"仅供测试");
+        //退款批准前
         Map<String,Object> order = (Map<String,Object>) orderService.getOrderInfo(order_id).getObject();
         String user_id = order.get("buyer_id").toString();
         Double price = (Double) order.get("price");
         User user = (User)userService.getById(user_id).getObject();
         Double balance_before = user.getBalance();
 
+        //退款批准后
         refundService.permitRefund(order_id);
         order = (Map<String,Object>) orderService.getOrderInfo(order_id).getObject();
         String order_state = order.get("order_state").toString();
         String isRefunding = order.get("isRefunding").toString();
+        Map<String,Object> refund = (Map<String,Object>) refundService.getRefundInfo(order_id).getObject();
+        String refund_state = refund.get("refund_state").toString();
         user = (User)userService.getById(user_id).getObject();
         Double balance_after = user.getBalance();
         Assert.assertEquals("已退款",order_state);
         Assert.assertEquals("n",isRefunding);
         Assert.assertTrue((balance_before+price)==balance_after);
+        Assert.assertEquals("卖家批准",refund_state);
     }
 
     @Transactional
     @Test
     void refuseRefund() {
+        //首先造一条退款申请
+        String order_id = "6631065311033663";
+        refundService.submitRefund(order_id,"仅供测试");
+        Map<String,Object> order = (Map<String,Object>) orderService.getOrderInfo(order_id).getObject();
+        String order_state_before = order.get("order_state").toString();
+
+        refundService.refuseRefund(order_id);
+        order = (Map<String,Object>) orderService.getOrderInfo(order_id).getObject();
+        Map<String,Object> refund = (Map<String,Object>) refundService.getRefundInfo(order_id).getObject();
+        String refund_state = refund.get("refund_state").toString();
+        String order_state_after = order.get("order_state").toString();
+        String isRefunding = order.get("isRefunding").toString();
+        Assert.assertEquals("卖家驳回",refund_state);
+        Assert.assertEquals(order_state_before,order_state_after);
+        Assert.assertEquals("y",isRefunding);
     }
 
+    /**
+     * 系统测试
+     */
+    @Transactional
     @Test
-    void submitArbitration() {
+    void RefundCase1() {
+        String order_id = "6631065311033663";
+        Assert.assertEquals(200,refundService.submitRefund(order_id,"仅供测试").getCode());
+        Assert.assertEquals(200,refundService.refuseRefund(order_id).getCode());
+        Assert.assertEquals(200,refundService.cancelRefund(order_id).getCode());
+        Assert.assertEquals(200,refundService.submitRefund(order_id,"仅供测试").getCode());
+        Assert.assertEquals(200,refundService.refuseRefund(order_id).getCode());
+        Assert.assertEquals(200,refundService.submitArbitration(order_id,"仅供测试",null).getCode());
+        Assert.assertEquals(200,refundService.permitArbitration(order_id).getCode());
     }
 
-    @Test
-    void permitArbitration() {
-    }
 
-    @Test
-    void refuseArbitration() {
-    }
 }
