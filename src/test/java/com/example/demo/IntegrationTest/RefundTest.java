f4150bb370ade4d66afb22590e4a713735006bc0
hthcccc <65483560+hthcccc@users.noreply.github.com>
2022-06-22 02:53:14 +0800

    no message

--- src/test/java/com/example/demo/IntegrationTest/RefundTest.java
+++ src/test/java/com/example/demo/IntegrationTest/RefundTest.java
@@ -122,50 +122,6 @@ public class RefundTest {
         Assert.assertEquals("已退款",order_state);
         Assert.assertEquals("n",isRefunding);
     }
-    /**
-     * 集成测试
-     * 初次提交退款——卖家驳回——买家取消——买家再次提交——卖家批准
-     */
-    @Transactional
-    @Test
-    public void RefundCase3() {
-        String order_id = "6631065311033663";
-        //获取测试前状态
-        Map<String,Object> order = (Map<String,Object>) orderService.getOrderInfo(order_id).getObject();
-        Double price = (Double) order.get("price");
-        String user_id = order.get("buyer_id").toString();
-        User user = (User) userService.getById(user_id).getObject();
-        Double balance_before = user.getBalance();
-        //流程测试
-        Assert.assertEquals(200,refundService.submitRefund(order_id,"仅供测试").getCode());
-        Map<String,Object> refund = (Map<String,Object>) refundService.getRefundInfo(order_id).getObject();
-        Assert.assertEquals("待审核",refund.get("refund_state").toString());
-
-        Assert.assertEquals(200,refundService.refuseRefund(order_id).getCode());
-        refund = (Map<String,Object>) refundService.getRefundInfo(order_id).getObject();
-        Assert.assertEquals("卖家驳回",refund.get("refund_state").toString());
-
-        Assert.assertEquals(200,refundService.cancelRefund(order_id).getCode());
-        Assert.assertNull(refundService.getRefundInfo(order_id).getObject());
-
-        Assert.assertEquals(200,refundService.submitRefund(order_id,"仅供测试").getCode());
-        refund = (Map<String,Object>) refundService.getRefundInfo(order_id).getObject();
-        Assert.assertEquals("待审核",refund.get("refund_state").toString());
-
-        Assert.assertEquals(200,refundService.permitRefund(order_id).getCode());
-        refund = (Map<String,Object>) refundService.getRefundInfo(order_id).getObject();
-        Assert.assertEquals("卖家批准",refund.get("refund_state").toString());
-
-        //获取测试后状态
-        order = (Map<String,Object>) orderService.getOrderInfo(order_id).getObject();
-        String order_state = order.get("order_state").toString();
-        String isRefunding = order.get("isRefunding").toString();
-        user = (User) userService.getById(user_id).getObject();
-        Double balance_after = user.getBalance();
-        Assert.assertTrue((balance_before+price)==balance_after);
-        Assert.assertEquals("已退款",order_state);
-        Assert.assertEquals("n",isRefunding);
-    }
 
     /**
      * 集成测试
